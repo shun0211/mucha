8ba88bce8d611a5aebaaf741a8aafc43d9ae6eaa
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-12-03 07:35:06 +0900

    ✨ ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post 'line_bot/callback', to: 'line_bot/callbacks#callback'
+  get 'line-bot/link', to: 'line_bot/callbacks#link'
+  scope module: :api do
+    scope module: :v1 do
+      get "healthcheck", to: "healthcheck#healthcheck"
+      resources :users, only: [:create]
+      resources :notices
+    end
+  end
 end
