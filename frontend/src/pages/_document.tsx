502dc5136c703d5e6b55d26f0f3b8e2406d87ff2
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-12-03 17:57:54 +0900

    ✨ document.tsxを追加
    全体で統一した背景色をつけたかったので、ここにbackground colorを設定した
    (ここで合ってるかは謎)

---
+++ frontend/src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import { createGetInitialProps } from "@mantine/next";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import React from "react";
+
+const getInitialProps = createGetInitialProps();
+
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps;
+
+  render() {
+    return (
+      <Html>
+        <Head>
+        </Head>
+        <body className="bg-bg-color">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
