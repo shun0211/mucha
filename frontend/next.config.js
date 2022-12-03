744669e3bd93b2f8b89dc5f1041d15c825d09885
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-12-03 16:55:44 +0900

    :seedling: 設定ファイル

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,4 +4,13 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+// Full Calendarはnode_modules内にCSSが含まれているため以下の設定が必要
+// https://qiita.com/tak001/items/b57459db58f6ff8658a4
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withTM = require("next-transpile-modules")([
+  "@fullcalendar/common",
+  "@fullcalendar/daygrid",
+  "@fullcalendar/react",
+]);
+
+module.exports = withTM(nextConfig);
