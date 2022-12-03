6192b1f8b748c34589a5442098954e74f9e1ab17
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-12-03 16:58:24 +0900

    🎨 Tailwind設定

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        'yellow': '#FFDC00',
+        'link-color': '#0073CC',
+        'hover-color': '#f8f9fa',
+        'bg-color': '#FAF9F4',
+        'white': '#FFFFFF',
+        'line-color': '#06c755'
+      },
       width: {}
     },
   },
