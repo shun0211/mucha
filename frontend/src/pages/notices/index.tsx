72ec650890c702b7e64858d7b7bcecf4f69938f0
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-12-04 08:26:00 +0900

    ✨ Pages追加

---
+++ frontend/src/pages/notices/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PagesNotices from '../../components/pages/notices/index.page'
+
+const Notices = () => {
+  return (
+    <PagesNotices />
+  )
+}
+
+export default Notices
---
+++ frontend/src/pages/notices/new.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PagesNoticesNew from '../../components/pages/notices/new.page'
+
+const New = () => {
+  return (
+    <PagesNoticesNew />
+  )
+}
+
+export default New
