a0d96178c202c9c014ee6485cd5d6510072a4d6e
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-12-03 17:59:16 +0900

    🎨 背景色を薄いグレーに変更
    さぶマネ！とできるだけ同じにしたいが、ここでは背景を変更したかった。
    全く同じである必要はないと思っているので、許容した。

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
         'yellow': '#FFDC00',
         'link-color': '#0073CC',
         'hover-color': '#f8f9fa',
-        'bg-color': '#FAF9F4',
+        'bg-color': '#F5F5F5',
         'white': '#FFFFFF',
         'line-color': '#06c755'
       },
