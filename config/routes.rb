0bb6a5bcfd1ce3c8a80a1de33b297a7e2412f89f
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-12-05 23:36:14 +0900

    🐛 APIのパスを/api/v1とする

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   post 'line_bot/callback', to: 'line_bot/callbacks#callback'
   get 'line-bot/link', to: 'line_bot/callbacks#link'
-  scope module: :api do
-    scope module: :v1 do
+  namespace 'api' do
+    namespace 'v1' do
       get "healthcheck", to: "healthcheck#healthcheck"
       resources :users, only: [:create]
       resources :notices
