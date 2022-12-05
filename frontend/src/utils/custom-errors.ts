ee7830c2883aa3702f6613ba511b817056800a33
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-12-05 23:37:28 +0900

    ⚡️ カスタムエラーを作成

---
+++ frontend/src/utils/custom-errors.ts
@@ -0,0 +1,39 @@
+import { ErrorMessage, ErrorMessages } from "../types";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function isErrorMessage(value: any): value is ErrorMessage {
+  // NOTE: プロパティがアクセスできない可能性を排除
+  if (value == null) return false;
+
+  return (
+    typeof value.message == "string" &&
+    typeof value.forDeveloperMessage == "string"
+  );
+}
+
+export function isErrorMessages(value: any): value is ErrorMessages {
+  return (
+    Array.isArray(value.errorMessages) &&
+    typeof value.forDeveloperMessage == "string"
+  )
+}
+
+export class UnauthorizedError extends Error {
+  errorMessage: string;
+
+  constructor(errorMessage: string) {
+    super()
+    this.name = "UnauthorizedError"
+    this.errorMessage = errorMessage
+  }
+}
+
+export class NotAcceptableError extends Error {
+  errorMessages: string[];
+
+  constructor(errorMessages: string[]) {
+    super()
+    this.name = "NotAcceptableError"
+    this.errorMessages = errorMessages
+  }
+}
