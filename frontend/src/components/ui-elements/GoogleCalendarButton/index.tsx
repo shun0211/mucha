1d18f36a02d32e608dd0d18f41f2d2392acb8e33
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-12-07 08:58:38 +0900

    🎨 Googleカレンダーボタン

---
+++ frontend/src/components/ui-elements/GoogleCalendarButton/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mantine/core";
+import React from "react";
+
+const GoogleCalendarButton = () => {
+  return (
+    <>
+      <Button className="bg-blue-500 text-white">Googleカレンダーと連携</Button>
+    </>
+  );
+};
+
+export default GoogleCalendarButton;
