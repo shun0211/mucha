b2bfd45485b3dbc380f9af5bbcdf4a4f0636c89b
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-12-07 09:00:53 +0900

    🎨 カレンダーのコンポーネント

---
+++ frontend/src/components/features/display-calender/index.tsx
@@ -0,0 +1,38 @@
+import FullCalendar from "@fullcalendar/react";
+import React from "react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import jaLocale from "@fullcalendar/core/locales/ja";
+import useScheduledNotices from "../../../hooks/useScheduledNotices";
+import { Notice } from "../../../types";
+
+const DisplayCalender = () => {
+  const data = useScheduledNotices(1);
+  const scheduledNotices = data.notices ?? null;
+  if (!scheduledNotices) return null;
+
+  return (
+    <>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        locale={jaLocale}
+        events={scheduledNotices.map((notice: Notice) => {
+          return { title: notice.title, start: notice.scheduledAt };
+        })}
+        contentHeight={'auto'}
+        titleFormat={{year: 'numeric', month: 'short'}}
+        headerToolbar={{
+          start: 'title',
+          center: '',
+          end: 'prev,next'
+        }}
+        height={50}
+        dayCellClassNames="bg-white leading-3"
+        dayMaxEvents={2}
+        dayCellContent={(e) => e.dayNumberText.replace('日', '')}
+      />
+    </>
+  );
+};
+
+export default DisplayCalender;
