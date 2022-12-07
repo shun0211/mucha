d7787e1ecf4dff57a7e200fcec474b78e0256f2e
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-12-07 09:01:27 +0900

    ✨ 通知取得のhooks作成

---
+++ frontend/src/hooks/useScheduledNotices.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import useSWR from "swr";
+import { API_URL } from "../config/constants";
+import { UseNotices } from "./types";
+
+axios.defaults.headers.common["Accept"] = "application/json";
+
+const fetcher = (url: string) =>
+  axios.get(url, { withCredentials: true }).then((res) => res.data);
+
+const useScheduledNotices = (
+  userId: number,
+  page = 1,
+  perPage = 10
+): UseNotices => {
+  const { data, error } = useSWR<UseNotices>(`${API_URL}/notices?userId=${userId}&page=${page}&perPage=${perPage}&status=scheduled`, fetcher);
+
+  return {
+    notices: data ? data.notices : undefined,
+    isLoading: !error && !data,
+    isError: !!error,
+  };
+};
+
+export default useScheduledNotices;
