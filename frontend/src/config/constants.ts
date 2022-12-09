bd47a03fe03b61644fdab6e95e7a450adb1fa915
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-12-09 21:15:22 +0900

    ✨ 定数追加

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -1,2 +1,3 @@
 export const FRONT_URI = process.env.NEXT_PUBLIC_FRONT_URI as string
-export const API_URL = process.env.NEXT_PUBLIC_API_ENDPOINT as string
+export const API_URL = process.env.NEXT_PUBLIC_API_URL as string
+export const API_DOMAIN = process.env.NEXT_PUBLIC_API_DOMAIN as string
