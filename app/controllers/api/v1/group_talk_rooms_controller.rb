8650c146f1d0b122858813d9ff108e39951a2b5b
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-12-09 21:09:25 +0900

    ✨ グループトークのエンドポイント作成

---
+++ app/controllers/api/v1/group_talk_rooms_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::GroupTalkRoomsController < ApplicationController
+  def create
+    res = line_bot_client.get_group_summary(params[:line_group_id])
+    line_group_info = JSON.parse(res.body)
+    group_talk_room = GroupTalkRoom.new(
+      line_group_id: line_group_info["groupId"],
+      line_name: line_group_info["groupName"],
+      line_profile_image_url: line_group_info["pictureUrl"]
+    )
+    group_talk_room.user = current_user
+    group_talk_room.save!
+  end
+
+  private def line_bot_client
+    @line_bot_client ||= Line::Bot::Client.new do |config|
+      config.channel_id = ENV['LINE_CHANNEL_ID']
+      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+    end
+  end
+end
