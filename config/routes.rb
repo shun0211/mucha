d92c521faa2ed567961decb9ed04ec0243097b66
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-12-09 21:12:14 +0900

    ✨ ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,23 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   post 'line_bot/callback', to: 'line_bot/callbacks#callback'
-  get 'line-bot/link', to: 'line_bot/callbacks#link'
+  get 'line_bot/link', to: 'line_bot/callbacks#link'
+  get "healthcheck", to: "healthcheck#healthcheck"
+
   namespace 'api' do
     namespace 'v1' do
-      get "healthcheck", to: "healthcheck#healthcheck"
       resources :users, only: [:create]
+      get 'current-user', to: 'users#show_current_user'
+      post 'login', to: 'auth#login'
+      delete 'logout', to: 'auth#logout'
       resources :notices
+      put 'notices/:id/draft', to: "notices#update_to_draft"
+      resources :group_talk_rooms
     end
+  end
+
+  namespace 'admin' do
+    mount Sidekiq::Web, at: '/sidekiq'
   end
 end
