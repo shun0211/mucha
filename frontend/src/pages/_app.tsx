bda9fe5f8fda816eae20b32124cf20ad451939de
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-12-09 21:20:17 +0900

    ✨ app.tsにログインに関する処理を追加

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,37 +1,81 @@
 import "../styles/globals.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
 import { Toaster } from "react-hot-toast";
+import { AuthContext } from "../providers/auth";
+import { User } from "../types";
+import { useRouter } from "next/router";
+import Skeleton from "../components/ui-elements/Skeleton";
+import { getCurrentUser } from "../hooks/getCurrentUser";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+  const router = useRouter();
+  const [currentUser, setCurrentUser] = useState<User | null | undefined>(
+    undefined
+  );
+  const [authChecking, setAuthChecking] = useState<boolean>(true);
+
+  useEffect(() => {
+    const inner = async () => {
+      try {
+        const user: User = await getCurrentUser();
+        setCurrentUser(user);
+      } catch {
+        setCurrentUser(null);
+      }
+      setAuthChecking(false);
+    };
+    inner();
+  }, []);
+
+  const component = (authChecking: boolean): React.ReactNode => {
+    if (
+      props.router.pathname === "/" ||
+      props.router.pathname === "/line-account-linkage" ||
+      props.router.pathname === "/notices/new" ||
+      props.router.pathname === "/notices"
+    ) {
+      return <Component {...pageProps} />;
+    } else {
+      // ログインしていないときは/signinにリダイレクト
+      if (!authChecking && currentUser === null) {
+        router.push("signin");
+      }
+      return authChecking ? <Skeleton /> : <Component {...pageProps} />;
+    }
+  };
+
   return (
     <>
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: "light",
-          colors: {
-            "yellow": [
-              "#FFDC00",
-              "#FFDC00",
-              "#FFDC00",
-              "#FFDC00",
-              "#FFDC00",
-              "#FFDC00",
-              "#FFDC00",
-              "#FFDC00",
-              "#FFDC00",
-              "#FFDC00",
-            ],
-          },
-        }}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
-      <Toaster />
+      <AuthContext.Provider value={{ currentUser, setCurrentUser }}>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            /** Put your mantine theme override here */
+            colorScheme: "light",
+            colors: {
+              yellow: [
+                "#FFDC00",
+                "#FFDC00",
+                "#FFDC00",
+                "#FFDC00",
+                "#FFDC00",
+                "#FFDC00",
+                "#FFDC00",
+                "#FFDC00",
+                "#FFDC00",
+                "#FFDC00",
+              ],
+            },
+          }}
+        >
+          {component(authChecking)}
+        </MantineProvider>
+        <Toaster />
+      </AuthContext.Provider>
     </>
   );
 }
