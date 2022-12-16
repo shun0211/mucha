ae3923e799f9dff2d0aa13919ce342b0494daa21
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-12-16 09:04:43 +0900

    :sparkles: ヘルプページの骨格つくる

---
+++ frontend/src/pages/help/group-talk-linkage/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PagesGroupTalkLinkage from '../../../components/pages/help/group-talk-linkage'
+
+const GroupTalkLinkage = () => {
+  return (
+    <PagesGroupTalkLinkage />
+  )
+}
+
+export default GroupTalkLinkage
---
+++ frontend/src/pages/help/line-account-linkage/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PagesLineAccountLinkage from '../../../components/pages/help/line-account-linkage'
+
+const LineAccountLinkage = () => {
+  return (
+    <PagesLineAccountLinkage />
+  )
+}
+
+export default LineAccountLinkage
