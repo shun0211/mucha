da53217de2df33328693119739d62cac477577d8
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-12-24 17:27:17 +0900

    :bug: 定数追加

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -4,3 +4,4 @@ export const API_DOMAIN = process.env.NEXT_PUBLIC_API_DOMAIN as string
 export const AUTH0_DOMAIN = process.env.NEXT_PUBLIC_AUTH0_DOMAIN as string
 export const AUTH0_CLIENT_ID = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID as string
 export const AUTH0_AUDIENCE = process.env.NEXT_PUBLIC_AUTH0_AUDIENCE as string
+export const OFFICIAL_ACCOUNT_LINE_ID = process.env.NEXT_PUBLIC_OFFICIAL_ACCOUNT_LINE_ID as string
