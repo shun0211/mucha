036068c1baa87e772092eefa368a2fce0be80a9b
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-12-24 17:26:41 +0900

    :sparkles: ログインボタン作成

--- frontend/src/components/ui-elements/LoginButton/index.tsx
+++ frontend/src/components/ui-elements/LoginButton/index.tsx
@@ -1,13 +1,19 @@
-import { useAuth0 } from '@auth0/auth0-react'
-import { Button } from '@mantine/core'
-import React from 'react'
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "@mantine/core";
+import React from "react";
 
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0()
+  const { loginWithRedirect } = useAuth0();
 
   return (
-    <Button onClick={() => loginWithRedirect()}>ログイン</Button>
-  )
-}
+    <Button
+      onClick={() => loginWithRedirect()}
+      fullWidth
+      className="bg-yellow text-black text-lg my-4 border-0"
+    >
+      ログイン
+    </Button>
+  );
+};
 
-export default LoginButton
+export default LoginButton;
