66a87b8d66cf76e2a3f3e1bd9b2ea8d2c4d3b17a
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2023-01-02 08:54:43 +0900

    :sparkles: ヘッダーのロゴとタイトルをクリックした際にトップページに遷移するようにする

--- frontend/src/components/ui-elements/Header/index.jsx
+++ frontend/src/components/ui-elements/Header/index.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Container, Title } from "@mantine/core";
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <>
-      <Container className="flex justify-center items-center py-3">
-        <Image src="/logo-only-image.png" alt="logo" width={35} height={35} />
-        <Title order={2} className="ml-3">
-          Mucha
-        </Title>
+      <Container className="py-3">
+        <Link href="/" className="flex justify-center items-center ">
+          <Image src="/logo-only-image-removebg.png" alt="logo" width={35} height={35} />
+          <Title order={2} className="ml-3">
+            Mucha
+          </Title>
+        </Link>
       </Container>
     </>
   );
