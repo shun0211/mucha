bfcf7aaa194e87b14730c202ace13c115ad5871c
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2023-01-02 07:47:17 +0900

    :sparkles: SEO追加

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { createGetInitialProps } from "@mantine/next";
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import { DefaultSeo } from "next-seo";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
 
 const getInitialProps = createGetInitialProps();
@@ -16,6 +17,7 @@ export default class _Document extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1, user-scalable=no"
           />
+          <title>Mucha</title>
           <link
             rel="apple-touch-icon"
             sizes="180x180"
@@ -34,18 +36,30 @@ export default class _Document extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
-          <link
-            rel="mask-icon"
-            href="/favicon/safari-pinned-tab.svg"
-            color="#000000"
-          />
-          <link rel="shortcut icon" href="/favicon/favicon.ico" />
-          <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta
-            name="msapplication-config"
-            content="/favicon/browserconfig.xml"
-          />
+          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
+          <DefaultSeo
+            defaultTitle="Mucha"
+            description="家族や恋人同士で使える便利なリマインドチャットツール"
+            canonical="https://muchualchat.com/"
+            openGraph={{
+              type: "website",
+              title: "Mucha",
+              description:
+                "家族や恋人同士で使える便利なリマインドチャットツール",
+              site_name: "Mucha",
+              url: "https://muchualchat.com/",
+              images: [
+                {
+                  url: "https://mucha.s3.ap-northeast-1.amazonaws.com/logo-only-image.png",
+                  width: 600,
+                  height: 600,
+                  alt: "Muchaのロゴ",
+                },
+              ],
+            }}
+          />
         </Head>
         <body className="bg-bg-color">
           <Main />
