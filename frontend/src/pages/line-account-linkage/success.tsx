ce2be035a1839064ccdff85133d76feeb780b900
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2023-01-02 11:05:56 +0900

    :sparkles: グループトークの登録成功画面作成

--- frontend/src/pages/line-account-linkage/success.tsx
+++ frontend/src/pages/line-account-linkage/success.tsx
@@ -1,8 +1,21 @@
+import { Container } from '@mantine/core'
 import React from 'react'
+import Header from '../../components/ui-elements/Header'
+import MainText from '../../components/ui-elements/MainText'
+import MainLinkButton from '../../components/ui-elements/MainLinkButton'
 
 const Success = () => {
   return (
-    <div>Success</div>
+    <div>
+      <Header />
+      <Container size={300} className="mt-5">
+        <MainText
+          text={`グループトークの登録に成功しました。
+            グループトークにメッセージを送信できます😳`}
+        />
+        <MainLinkButton text='今すぐリマインドを設定する👆' src='/notices/new' />
+      </Container>
+    </div>
   )
 }
 
