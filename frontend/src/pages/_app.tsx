d23eccda30cb561bdde81ca259bc6507e1927f05
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2023-01-02 15:09:00 +0900

    :zap: DefaultSeo を _app.tsx に移す

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,18 +3,35 @@ import React from "react";
 import type { AppProps } from "next/app";
 import MuchaAuthProvider from "../providers/MuchaAuthProvider";
 import Mucha from "../components/Mucha";
+import { DefaultSeo } from "next-seo";
 
 export default function App(props: AppProps) {
   const { Component, pageProps, router } = props;
 
   return (
     <>
+      <DefaultSeo
+        defaultTitle="Mucha"
+        description="家族や恋人同士で使える便利なリマインドチャットツール"
+        canonical="https://muchualchat.com/"
+        openGraph={{
+          type: "website",
+          title: "Mucha",
+          description: "家族や恋人同士で使える便利なリマインドチャットツール",
+          site_name: "Mucha",
+          url: "https://muchualchat.com/",
+          images: [
+            {
+              url: "https://mucha.s3.ap-northeast-1.amazonaws.com/logo-only-image.png",
+              width: 600,
+              height: 600,
+              alt: "Muchaのロゴ",
+            },
+          ],
+        }}
+      />
       <MuchaAuthProvider>
-        <Mucha
-          Component={Component}
-          pageProps={pageProps}
-          router={router}
-        />
+        <Mucha Component={Component} pageProps={pageProps} router={router} />
       </MuchaAuthProvider>
     </>
   );
--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -12,12 +12,11 @@ export default class _Document extends Document {
     return (
       <Html>
         <Head>
-          {/* iPhoneのsafariでフォーム入力の時に自動的に画面拡大するのを防ぐ */}
+          <title>Mucha</title>
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1, user-scalable=no"
           />
-          <title>Mucha</title>
           <link
             rel="apple-touch-icon"
             sizes="180x180"
@@ -36,30 +35,13 @@ export default class _Document extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
-          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+          <link
+            rel="mask-icon"
+            href="/favicon/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
-          <DefaultSeo
-            defaultTitle="Mucha"
-            description="家族や恋人同士で使える便利なリマインドチャットツール"
-            canonical="https://muchualchat.com/"
-            openGraph={{
-              type: "website",
-              title: "Mucha",
-              description:
-                "家族や恋人同士で使える便利なリマインドチャットツール",
-              site_name: "Mucha",
-              url: "https://muchualchat.com/",
-              images: [
-                {
-                  url: "https://mucha.s3.ap-northeast-1.amazonaws.com/logo-only-image.png",
-                  width: 600,
-                  height: 600,
-                  alt: "Muchaのロゴ",
-                },
-              ],
-            }}
-          />
         </Head>
         <body className="bg-bg-color">
           <Main />
