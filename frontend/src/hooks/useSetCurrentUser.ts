5559bd86643ee4bd6761b55bf30610f16c1efe9d
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2023-01-15 19:30:36 +0900

    :bug: Test

--- frontend/src/hooks/useSetCurrentUser.ts
+++ frontend/src/hooks/useSetCurrentUser.ts
@@ -29,7 +29,7 @@ export const useSetCurrentUser = (
         const liffLogin = async () => {
           const accessToken = liff.getAccessToken();
           console.log(accessToken);
-          const customToken = await axios.get(`${API_URL}/custom-token`, {
+          const customToken = await axios.get(`${API_URL}/liff/custom-token`, {
             headers: {
               Authorization: `Bearer ${token}`,
             },
