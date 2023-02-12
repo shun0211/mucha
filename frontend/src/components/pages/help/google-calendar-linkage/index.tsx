638daad1dc3b96f494f63b6f30be4ef5937936cd
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2023-02-12 15:34:04 +0900

    📝 Google カレンダー連携の警告に関するヘルプページ追加

---
+++ frontend/src/components/pages/help/google-calendar-linkage/index.tsx
@@ -0,0 +1,29 @@
+import { Container, Text } from "@mantine/core";
+import Image from "next/image";
+import React from "react";
+import Header from "../../../ui-elements/Header";
+import PageTitle from "../../../ui-elements/PageTitle";
+
+const PagesGoogleCalendarLinkage = () => {
+  return (
+    <>
+      <Header />
+      <PageTitle>Google カレンダー連携について</PageTitle>
+      <Container size={300}>
+        <Text fw={700} className="whitespace-pre-wrap">
+          {`現在、Google の審査中のため以下の画面が表示されます。\n\nMucha を信頼して頂ける場合は、左下の「詳細」をクリックして「muchualchat.com (安全ではないページ)に移動」をクリックしてください。`}
+        </Text>
+
+        <Image
+          src="/google-calendar-alert.png"
+          alt="google calendar linkage note"
+          width={300}
+          height={100}
+          className="mx-auto py-5"
+        />
+      </Container>
+    </>
+  );
+};
+
+export default PagesGoogleCalendarLinkage;
---
+++ frontend/src/pages/help/google-calendar-linkage/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PagesGoogleCalendarLinkage from '../../../components/pages/help/google-calendar-linkage'
+
+const GoogleCalendarLinkage = () => {
+  return (
+    <PagesGoogleCalendarLinkage />
+  )
+}
+
+export default GoogleCalendarLinkage
