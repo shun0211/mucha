77e2b3d6a86aa8f3d45462f26a08432fe7cafa9d
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2023-02-12 15:32:30 +0900

    🚀 Google カレンダーボタン追加

--- frontend/src/components/pages/notices/index.page.tsx
+++ frontend/src/components/pages/notices/index.page.tsx
@@ -1,8 +1,8 @@
-import { Container } from "@mantine/core";
+import { Anchor, Container, Text } from "@mantine/core";
 import React, { useContext } from "react";
 import Header from "../../ui-elements/Header";
 import PageTitle from "../../ui-elements/PageTitle";
-// import GoogleCalendarButton from "../../ui-elements/GoogleCalendarButton";
+import GoogleCalendarButton from "../../ui-elements/GoogleCalendarButton";
 import NoticeTarget from "../../ui-elements/NoticeTarget";
 import DisplayCalender from "../../features/calendar/components/DisplayCalendar";
 import { AuthContext } from "../../../providers/AuthContext";
@@ -21,9 +21,18 @@ const PagesNotices = () => {
         <PageTitle>リマインド一覧</PageTitle>
 
         <DisplayCalender user={currentUser} token={token} />
-        <div className="flex justify-between items-center py-3">
-          <div></div>
-          {/* <GoogleCalendarButton /> */}
+        <div className="flex justify-between items-end py-3">
+          <div>
+            <Text fz="xs">※ 開発中の機能のため警告画面が出ます</Text>
+            <Text fz="xs" className="pb-1">
+              &emsp;詳しくは
+              <Anchor href="/help/google-calendar-linkage" target="_blank">
+                こちら
+              </Anchor>
+              をご確認ください
+            </Text>
+            <GoogleCalendarButton token={token} />
+          </div>
           <NoticeTarget user={currentUser} token={token} />
         </div>
         <DisplayNotices user={currentUser} token={token} />
--- frontend/src/components/ui-elements/GoogleCalendarButton/index.tsx
+++ frontend/src/components/ui-elements/GoogleCalendarButton/index.tsx
@@ -1,10 +1,32 @@
 import { Button } from "@mantine/core";
+import axios from "axios";
 import React from "react";
+import { API_URL } from "../../../config/constants";
 
-const GoogleCalendarButton = () => {
+const GoogleCalendarButton = ({ token }: { token: string }) => {
+  const syncCalendar = async () => {
+    const res = await axios.get(`${API_URL}/google_calendar/authorize`, {
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const redirectUrl = res.data.redirectUrl;
+    window.location.href = `${redirectUrl}?test=aaa`;
+  };
+
   return (
     <>
-      <Button className="bg-blue-500 text-white">Googleカレンダーと連携</Button>
+      <Button
+        className="bg-blue-500 text-white"
+        onClick={() => {
+          if (window.confirm('開発中の機能のため警告画面が出ます\n詳しくはこちらをご確認ください\nhttps://google.com')) {
+            syncCalendar();
+          }
+        }}
+      >
+        Googleカレンダーと連携
+      </Button>
     </>
   );
 };
