6473fc6d22db132182150e5a4b075221de825c02
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2023-02-13 22:01:42 +0900

    🚑 ログインできないのを解消

--- frontend/src/hooks/addFriend.ts
+++ frontend/src/hooks/addFriend.ts
@@ -4,9 +4,13 @@ import { API_URL } from "../config/constants";
 axios.defaults.headers.common["Accept"] = "application/json";
 
 export const addFriend = async (userId: number, token: string) => {
-  await axios.put(`${API_URL}/users/${userId}/follow`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  await axios.put(
+    `${API_URL}/users/${userId}/follow`,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
 };
