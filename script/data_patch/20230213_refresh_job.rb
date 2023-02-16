21c107f53ab3f0c665046cf2c9ca90b11a5f360a
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2023-02-17 07:37:22 +0900

    ジョブをデータベースのレコードから再作成する DataPatch 作成

---
+++ script/data_patch/20230213_refresh_job.rb
@@ -0,0 +1,43 @@
+# USAGE:
+# bundle exec rails runner 'load "script/data_patch/20230213_refresh_job.rb"; DataPatch::RefreshJob.new.execute!'
+
+module DataPatch
+  class RefreshJob
+    def initialize
+      @logger = Logger.new("#{Rails.root}/refresh_job.log")
+    end
+
+    def execute!
+      scheduled_line_message_jobs = LineMessageJob.where(scheduled_at: Time.current..).to_a
+      ss = Sidekiq::ScheduledSet.new
+
+      puts '【実行前】'
+      puts "LineMessage #{scheduled_line_message_jobs.count}件"
+      puts "登録されている Job #{ss.count}件"
+
+      puts "処理を開始します ... [y/n]"
+      exit if STDIN.gets.strip != 'y'
+      puts '****************************************************************************************************'
+      puts '【処理開始】'
+      @logger.info('Start')
+
+      puts 'Job の削除'
+      ss.clear
+
+      puts 'Job の登録'
+      scheduled_line_message_jobs.each do |job|
+        SendLineMessageJob
+          .set(wait_until: job.scheduled_at)
+          .perform_async(job.notice_id)
+      end
+
+      puts '【実行後】'
+      puts "LineMessage #{scheduled_line_message_jobs.count}件"
+      puts "登録されている Job #{ss.count}件"
+
+      puts '【処理完了】'
+      puts '****************************************************************************************************'
+      @logger.info('End')
+    end
+  end
+end
