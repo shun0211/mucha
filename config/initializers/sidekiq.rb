83dcc73c1ef12550824d752ca53409d8dde4b18d
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2023-02-17 22:25:04 +0900

    Sidekiq::Web の Basic 認証修正

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,14 @@
+require 'sidekiq/web'
+
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'] }
+end
+
+# Basic 認証
+Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+  [user, password] == [ENV['SIDEKIQ_WEB_BASIC_AUTHENTICATION_USER'], ENV['SIDEKIQ_WEB_BASIC_AUTHENTICATION_PASSWORD']]
 end
