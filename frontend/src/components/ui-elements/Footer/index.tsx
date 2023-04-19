b30f743ca066f4a722cc25a234196f4c7a9f9281
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2023-04-19 21:53:15 +0900

    フッター完了

--- frontend/src/components/ui-elements/Footer/index.tsx
+++ frontend/src/components/ui-elements/Footer/index.tsx
@@ -5,25 +5,31 @@ import { CONTACT_URL } from "../../../config/constants";
 const Footer = () => {
   return (
     <>
-      <Container className="bg-slate-600 text-center pt-3 pb-5">
-        <Text color="white" fz="xl" fw="bold">
-          Mucha
+      <Container className="bg-accent text-center pt-3 pb-5">
+        <Text color="#7E7E7E" fz="xl" fw="bold">
+          MUCHA
         </Text>
-        <Anchor href={CONTACT_URL} target="_blank" className="text-white block">
-          お問い合わせ
-        </Anchor>
-        <Anchor href="/terms" className="text-white block">
+        <Anchor href="/terms" className="text-[#545454] block pt-1" size="sm">
           利用規約
         </Anchor>
-        <Anchor href="/privacy" className="text-white block">
+        <Anchor href="/privacy" className="text-[#545454] block pt-1" size="sm">
           プライバシーポリシー
+        </Anchor>
+        <Anchor
+          href={CONTACT_URL}
+          target="_blank"
+          size="sm"
+          className="text-[#545454] block pt-1"
+        >
+          お問い合わせ
         </Anchor>
         <Anchor
           href="https://sakaishun.com"
           target="_blank"
-          className="text-white block"
+          size="sm"
+          className="text-[#545454] block pt-1"
         >
-          Blog
+          開発者ブログ
         </Anchor>
       </Container>
     </>
