657e0a342b0ebbbec488bbc8e5fda54c65132d2b
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2023-09-02 13:07:48 +0900

    日付をクリックすると予定が表示される

--- frontend/src/components/features/calendar/components/DisplayCalendar.tsx
+++ frontend/src/components/features/calendar/components/DisplayCalendar.tsx
@@ -3,9 +3,11 @@ import React from "react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import jaLocale from "@fullcalendar/core/locales/ja";
-import { User } from "../../../../types";
+import { Notice, User } from "../../../../types";
 import useScheduledNotices from "../../../../hooks/useScheduledNotices";
 import dayjs from "dayjs";
+import { Modal } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 
 type CalendarEvent = {
   title: string;
@@ -13,6 +15,9 @@ type CalendarEvent = {
 };
 
 const DisplayCalender = ({ user, token }: { user: User; token: string }) => {
+  const [opened, { open, close }] = useDisclosure(false);
+  const [todaysNotices, setTodaysNotices] = React.useState<Notice[]>([]);
+
   const data = useScheduledNotices(user.id, token);
   const scheduledNotices = data.notices ?? null;
   if (!scheduledNotices) return null;
@@ -23,13 +28,38 @@ const DisplayCalender = ({ user, token }: { user: User; token: string }) => {
     events.push.apply(
       events,
       notice.scheduledDatetimes.map((datetime) => {
-        return { title: notice.title, start: dayjs(datetime).format("YYYY-MM-DD") };
+        return {
+          title: notice.title,
+          start: dayjs(datetime).format("YYYY-MM-DD"),
+        };
       })
     );
   });
 
+  const openModal = (date: Date) => {
+    const tmpTodaysNotices = scheduledNotices.filter((notice) => {
+      return new Date(notice.scheduledDate).getTime() === date.getTime();
+    });
+
+    if (tmpTodaysNotices.length > 0) {
+      setTodaysNotices(tmpTodaysNotices);
+      open();
+    }
+  };
+
   return (
     <>
+      <Modal opened={opened} onClose={close} withCloseButton={false}>
+        {todaysNotices.length > 0 &&
+          todaysNotices.map((notice) => {
+            return (
+              <div key={notice.id} className="flex">
+                <p>{notice.title}</p>
+                <p className="pl-3">{notice.scheduledTime}</p>
+              </div>
+            );
+          })}
+      </Modal>
       <FullCalendar
         plugins={[dayGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
@@ -46,7 +76,14 @@ const DisplayCalender = ({ user, token }: { user: User; token: string }) => {
         dayCellClassNames="bg-white leading-3"
         dayMaxEvents={2}
         dayCellContent={(e) => e.dayNumberText.replace("日", "")}
-        // dateClick={(selectionInfo) => console.log(selectionInfo)}
+        dateClick={(selectionInfo) => {
+          openModal(selectionInfo.date);
+        }}
+        eventClick={(e) => {
+          if (e.event.start) {
+            openModal(e.event.start);
+          }
+        }}
       />
     </>
   );
